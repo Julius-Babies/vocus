59f626e7a500499943a81fd14ee77628da9e7a98
Julius Vincent Babies <julius@familie-babies.de>
2025-08-30 19:57:49 +0200

    Handle null values in container port retrieval to prevent exceptions

--- src/main/kotlin/utils/docker/getExposedPorts.kt
+++ src/main/kotlin/utils/docker/getExposedPorts.kt
@@ -3,7 +3,7 @@ package dev.babies.utils.docker
 import com.github.dockerjava.api.model.Container
 
 fun Container.getExposedPorts(): Map<Int, Int> {
-    val ports = ports
+    val ports = ports.orEmpty()
         .associate { it.publicPort to it.privatePort }
         .filterKeys { it != null }
         .filterValues { it != null }
