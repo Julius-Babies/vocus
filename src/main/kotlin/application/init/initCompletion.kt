0b3697deab978747ab6a8cfe153a022c2315014a
Julius Vincent Babies <julius@familie-babies.de>
2025-09-07 17:16:52 +0200

    Enhance initCompletion feedback with error handling and completion message

--- src/main/kotlin/application/init/initCompletion.kt
+++ src/main/kotlin/application/init/initCompletion.kt
@@ -7,6 +7,7 @@ import dev.babies.utils.gray
 import dev.babies.utils.green
 import dev.babies.utils.red
 import dev.babies.utils.yellow
+import java.nio.charset.StandardCharsets
 import kotlin.system.exitProcess
 
 fun initCompletion(
@@ -43,6 +44,10 @@ fun initCompletion(
             println(gray("> ") + green("$currentShell $reloadCommand"))
             val process = ProcessBuilder(currentShell, reloadCommand).inheritIO().start()
             process.waitFor()
+            println(red(process.errorStream.bufferedReader().readText()))
+            println(green(process.inputStream.bufferedReader().readText()))
+            println("If you receive an error like " + yellow("/bin/zsh: can't open input file: ~/.vocus/autocomplete.zsh") + " and you are using macOS, make sure to grant the java binary full disk access in system settings. See " + yellow("https://apple.stackexchange.com/questions/402132/cronjobs-do-not-run/402179#402179") + " for a similar problem.")
+            println("Reload complete.")
         } else {
             println(yellow("Please reload your shell to use autocomplete"))
         }
