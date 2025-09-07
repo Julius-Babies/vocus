112552b691d690ca296fa928e979a2a9bccd9e8b
Julius Vincent Babies <julius@familie-babies.de>
2025-09-07 17:02:18 +0200

    Update ProjectItemCommand to use domain name conversion for project initialization

--- src/main/kotlin/application/cli/project/item/ProjectItemCommand.kt
+++ src/main/kotlin/application/cli/project/item/ProjectItemCommand.kt
@@ -4,10 +4,11 @@ import com.github.ajalt.clikt.command.SuspendingCliktCommand
 import com.github.ajalt.clikt.core.subcommands
 import dev.babies.application.cli.project.ProjectCommandContext
 import dev.babies.application.cli.project.item.module.ModuleCommand
+import dev.babies.application.os.host.DomainBuilder
 
 class ProjectItemCommand(
     commandContext: ProjectCommandContext,
-) : SuspendingCliktCommand(commandContext.project.name) {
+) : SuspendingCliktCommand(DomainBuilder.nameToDomain(commandContext.project.name)) {
     override suspend fun run() {}
 
     init {
